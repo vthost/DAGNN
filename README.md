68cc57e5ee9579e8ce5fc032a3909636ac3f1ef6
vthost <veronika.thost@gmail.com>
2021-02-03 13:46:31 -0500

    update readme

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ The DAGNN models in this directory contain additional methods to support the dec
 * `/ogb` <br/>Basically the code from the [Open Graph Benchmark (OGB)](https://github.com/snap-stanford/ogb). We just added additional data preprocessing steps necessary for DAGNN.
 * `/ogbg-code` <br/>Experiment code for TOK and LP experiments over the `ogbg-code` data from OGB. The [DAGNN model](ogbg-code/models/dagnn.py) in this directory, can be considered as the basic implementation of DAGNN.
 * `/scripts` <br/>Scripts for running the experiments.
-* `/src` <br/> Basic utilities used in all experiments. 
+* `/src` <br/> Basic utilities used in all experiments.
 
 ## OGB Experiments
 
---
+++ src/utils_dag.py
@@ -0,0 +1,76 @@
+import torch
+import numpy
+
+
+# see https://github.com/unbounce/pytorch-tree-lstm/blob/66f29a44e98c7332661b57d22501107bcb193f90/treelstm/util.py#L8
+# assume nodes consecutively named starting at 0
+#
+def top_sort(edge_index, graph_size):
+
+    node_ids = numpy.arange(graph_size, dtype=int)
+
+    node_order = numpy.zeros(graph_size, dtype=int)
+    unevaluated_nodes = numpy.ones(graph_size, dtype=bool)
+
+    parent_nodes = edge_index[0]
+    child_nodes = edge_index[1]
+
+    n = 0
+    while unevaluated_nodes.any():
+        # Find which parent nodes have not been evaluated
+        unevaluated_mask = unevaluated_nodes[parent_nodes]
+
+        # Find the child nodes of unevaluated parents
+        unready_children = child_nodes[unevaluated_mask]
+
+        # Mark nodes that have not yet been evaluated
+        # and which are not in the list of children with unevaluated parent nodes
+        nodes_to_evaluate = unevaluated_nodes & ~numpy.isin(node_ids, unready_children)
+
+        node_order[nodes_to_evaluate] = n
+        unevaluated_nodes[nodes_to_evaluate] = False
+
+        n += 1
+
+    return torch.from_numpy(node_order).long()
+
+
+# to be able to use pyg's batch split everything into 1-dim tensors
+def add_order_info_01(graph):
+
+    l0 = top_sort(graph.edge_index, graph.num_nodes)
+    ei2 = torch.LongTensor([list(graph.edge_index[1]), list(graph.edge_index[0])])
+    l1 = top_sort(ei2, graph.num_nodes)
+    ns = torch.LongTensor([i for i in range(graph.num_nodes)])
+
+    graph.__setattr__("_bi_layer_idx0", l0)
+    graph.__setattr__("_bi_layer_index0", ns)
+    graph.__setattr__("_bi_layer_idx1", l1)
+    graph.__setattr__("_bi_layer_index1", ns)
+
+    assert_order(graph.edge_index, l0, ns)
+    assert_order(ei2, l1, ns)
+
+
+def assert_order(edge_index, o, ns):
+    # already processed
+    proc = []
+    for i in range(max(o)+1):
+        # nodes in position i in order
+        l = o == i
+        l = ns[l].tolist()
+        for n in l:
+            # predecessors
+            ps = edge_index[0][edge_index[1] == n].tolist()
+            for p in ps:
+                assert p in proc
+        proc += l
+
+
+def add_order_info(graph):
+    ns = torch.LongTensor([i for i in range(graph.num_nodes)])
+    layers = torch.stack([top_sort(graph.edge_index, graph.num_nodes), ns], dim=0)
+    ei2 = torch.LongTensor([list(graph.edge_index[1]), list(graph.edge_index[0])])
+    layers2 = torch.stack([top_sort(ei2, graph.num_nodes), ns], dim=0)
+
+    graph.__setattr__("bi_layer_index", torch.stack([layers, layers2], dim=0))
